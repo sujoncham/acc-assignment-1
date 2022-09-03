fcb0cdfb37c247ef8dceab883c30183dcc659382
sujon-chambugong <sujonchambugong@gmail.com>
2022-09-03 17:47:57 +0600

    vercel.json config added

--- index.js
+++ index.js
@@ -39,7 +39,35 @@ app.post('/user/saveUser', (req, res)=>{
     res.send('Invalid data');
 });
 
+app.patch('/user/updateUser/:id', (req, res)=>{
+    const {id} = req.params;
+    const filter = {_id:id};
+    const userUpdate = users.find(user => user.id === Number(id));
 
+
+    
+    userUpdate.id = id,
+    userUpdate.name = req.body.name,
+    userUpdate.contact = req.body.contact,
+    userUpdate.gender = req.body.gender,
+    userUpdate.address = req.body.address,
+    userUpdate.job = req.body.job,
+    userUpdate.photoUrl = req.body.photoUrl,
+    
+   
+    res.send(userUpdate)
+});
+
+// app.patch('/user/bulk-update', (req, res)=>{
+//     res.send('random number')
+// });
+
+app.delete('/user/delete/:id', (req, res)=>{
+    const {id} = req.params;
+    const filter = {_id:id};
+    const userDel = users.filter(user => user.id !== Number(id));
+    res.send(userDel);
+})
 
 app.listen(port, ()=>{
     console.log("our server is", port);
